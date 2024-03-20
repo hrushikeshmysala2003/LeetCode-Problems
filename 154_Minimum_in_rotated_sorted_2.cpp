8a3f863919a0c0c9aea2add150b419ad08f43776
rushi <hrushikeshmysala2003@gmail.com>
2024-03-20 13:34:39 +0530

    153 154 Minimum in rotated sorted array 1 and 2

---
+++ 154_Minimum_in_rotated_sorted_2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        int low = 0;
+        int high = nums.size() - 1;
+        int mini = INT_MAX;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (nums[low] == nums[mid] and nums[mid] == nums[high])
+            {
+                low++;
+                high--;
+                mini = min(mini, nums[mid]);
+                continue;
+            }
+            if (nums[low] <= nums[mid])
+            {
+                mini = min(mini, nums[low]);
+                low = mid + 1;
+            }
+            else
+            {
+                mini = min(mini, nums[mid]);
+                high = mid - 1;
+            }
+        }
+        return mini;
+    }
+};
+
+int main()
+{
+    return 0;
+}
